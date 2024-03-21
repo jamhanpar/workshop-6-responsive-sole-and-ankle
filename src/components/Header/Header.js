f53a8d3f4ed0cf38b1e20d9e25a92caa8c8596b5
Jamhanpar <james.hansung.park@gmail.com>
2024-03-21 17:38:04 -0400

    responsive nav and body elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import VisuallyHidden from '../VisuallyHidden';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -17,19 +20,34 @@ const Header = () => {
   return (
     <header>
       <SuperHeader />
-      <MainHeader>
-        <Side>
+      <MainHeader showMobileMenu={showMobileMenu}>
+        <LogoWrapper>
           <Logo />
-        </Side>
+        </LogoWrapper>
         <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
+          <NavLink href='/sale'>Sale</NavLink>
+          <NavLink href='/new'>New&nbsp;Releases</NavLink>
+          <NavLink href='/men'>Men</NavLink>
+          <NavLink href='/women'>Women</NavLink>
+          <NavLink href='/kids'>Kids</NavLink>
+          <NavLink href='/collections'>Collections</NavLink>
         </Nav>
-        <Side />
+
+        <MobileActions>
+          <UnstyledButton>
+            <VisuallyHidden>Open Cart</VisuallyHidden>
+            <Icon id='shopping-bag' strokeWidth={1} />
+          </UnstyledButton>
+          <UnstyledButton>
+            <VisuallyHidden>Search</VisuallyHidden>
+            <Icon id='search' strokeWidth={1} />
+          </UnstyledButton>
+          <UnstyledButton>
+            <VisuallyHidden>Open Menu</VisuallyHidden>
+            <Icon id='menu' strokeWidth={1} />
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +64,56 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    border-top: 4px solid ${COLORS.gray[900]};
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
-const Side = styled.div`
+const MobileActions = styled.nav`
+  display: none;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    gap: 16px;
+  }
+`;
+
+const LogoWrapper = styled.div`
   flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -16,17 +16,17 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
     <div>
       <button onClick={onDismiss}>Dismiss menu</button>
       <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
+        <a href='/sale'>Sale</a>
+        <a href='/new'>New&nbsp;Releases</a>
+        <a href='/men'>Men</a>
+        <a href='/women'>Women</a>
+        <a href='/kids'>Kids</a>
+        <a href='/collections'>Collections</a>
       </nav>
       <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
+        <a href='/terms'>Terms and Conditions</a>
+        <a href='/privacy'>Privacy Policy</a>
+        <a href='/contact'>Contact Us</a>
       </footer>
     </div>
   );
